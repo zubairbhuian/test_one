614ffe5192a9ff195cd1044a70698ed1bb7a39b0
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-27 16:50:33 +0600

    test.5

---
+++ lib/const/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary_color = Colors.blue;
+  static const Color primary_light_color = Color.fromRGBO(13, 110, 253, 1);
+  static const Color secondary_color = Color.fromARGB(255, 233, 125, 118);
+  static const Color secondarylight_color = Color.fromARGB(255, 233, 125, 118);
+  static const Color success_color = Colors.green;
+  static const Color danger_color = Colors.red;
+  static const Color warning_color = Color.fromARGB(255, 245, 172, 3);
+  static const Color info_color = Colors.blueAccent;
+  static const Color light_color = Colors.lightBlue;
+  static const Color dark_color = Colors.black;
+}
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_one/const/app_color.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,10 +11,10 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
-          backgroundColor: Colors.amber[300],
-          body: const Center(
+          backgroundColor: AppColors.primary_light_color,
+          body: Center(
             child: CircularProgressIndicator(
               backgroundColor: Colors.white,
               strokeWidth: 5,
